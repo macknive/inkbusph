dc62b85fb356e9e5a0c3c671d7a4130c5adca7c4
macknive <kevin.corpuz@virtusize.com>
2024-04-13 23:00:22 +0800

    update base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "https://macknive.github.io/inkbusph/",
   plugins: [vue()],
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
